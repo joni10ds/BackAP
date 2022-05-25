b56d7afa02dab984c987af974712d96c9b3c4a9a
joni10ds <jonathan.schonholz@gmail.com>
2022-05-25 12:36:17 -0300

    3 commit

--- SpringB/src/main/java/com/porfolio/SpringB/Controller/Controller.java
+++ SpringB/src/main/java/com/porfolio/SpringB/Controller/Controller.java
@@ -1,8 +1,14 @@
 package com.porfolio.SpringB.Controller;
+import com.porfolio.SpringB.model.Educacion;
 import com.porfolio.SpringB.model.Experiencia;
+import com.porfolio.SpringB.model.Idioma;
 import com.porfolio.SpringB.model.Persona;
+import com.porfolio.SpringB.model.Skill;
+import com.porfolio.SpringB.service.IEducacionService;
 import com.porfolio.SpringB.service.IExperienciaService;
+import com.porfolio.SpringB.service.IIdiomaService;
 import com.porfolio.SpringB.service.IPersonaService;
+import com.porfolio.SpringB.service.ISkillService;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,5 +64,66 @@ public class Controller {
     public void borrarExperiencia (@PathVariable Long id){
         expServ.borrarExperiencia(id);
     }
+
+  //Educaciones 
+    @Autowired
+    private IEducacionService eduServ;
     
-}
+    @PostMapping ("/new/educacion")
+    public void agregarEducacion (@RequestBody Educacion exp){
+        eduServ.crearEducacion(exp);
+    }
+    
+    @GetMapping ("/ver/educaciones")
+    @ResponseBody
+    public List<Educacion> verEducaciones() {
+        return eduServ.verEducaciones();
+    }
+ 
+    @DeleteMapping ("/delete/educacion/{id}")
+    public void borrarEducacion (@PathVariable Long id){
+        eduServ.borrarEducacion(id);
+    }
+    
+    //Idiomas
+    @Autowired
+    private IIdiomaService idiServ;
+    
+    @PostMapping ("/new/idioma")
+    public void agregarIdioma (@RequestBody Idioma exp){
+        idiServ.crearIdioma(exp);
+    }
+    
+    @GetMapping ("/ver/idiomas")
+    @ResponseBody
+    public List<Idioma> verIdiomas() {
+        return idiServ.verIdiomas();
+    }
+ 
+    @DeleteMapping ("/delete/idioma/{id}")
+    public void borrarIdioma (@PathVariable Long id){
+        idiServ.borrarIdioma(id);
+    }
+    
+    //Skills
+    @Autowired
+    private ISkillService skiServ;
+    
+    @PostMapping ("/new/skill")
+    public void agregarSkill (@RequestBody Skill exp){
+        skiServ.crearSkill(exp);
+    }
+    
+    @GetMapping ("/ver/skills")
+    @ResponseBody
+    public List<Skill> verSkills() {
+        return skiServ.verSkills();
+    }
+ 
+    @DeleteMapping ("/delete/skill/{id}")
+    public void borrarSkill (@PathVariable Long id){
+        skiServ.borrarSkill(id);
+    }
+
+    }
+    
---
+++ SpringB/src/main/java/com/porfolio/SpringB/service/IEducacionService.java
@@ -0,0 +1,19 @@
+
+package com.porfolio.SpringB.service;
+
+import com.porfolio.SpringB.model.Educacion;
+import java.util.List;
+
+
+public interface IEducacionService {
+ 
+    
+    public List<Educacion> verEducaciones ();
+ 
+    public void crearEducacion (Educacion exp);
+    
+    public void borrarEducacion (Long id);
+    
+    public Educacion buscarEducacion (Long id);
+    
+}
